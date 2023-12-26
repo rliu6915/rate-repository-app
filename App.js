11507129be1ca7c611e9277fce18d9ea2dea22ae
rliu6915 <rliu6915@connect.hku.hk>
2023-12-26 11:36:32 +0800

    Exercise 10.11

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import Main from "./src/components/Main"
 import { NativeRouter } from 'react-router-native'
 
+import createApolloClient from './src/utils/apolloClient';
+import { ApolloProvider } from '@apollo/client';
+
+const apolloClient = createApolloClient();
+
 const App = () => {
   return (
     <>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
     </>
