d86f4c27a402619b78b093c7c3b828492a575d19
rliu6915 <rliu6915@connect.hku.hk>
2024-01-02 23:47:13 +0800

    Exercise 10.16: sign out

--- src/components/AppBar/AppBarTab.jsx
+++ src/components/AppBar/AppBarTab.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, StatusBar, View} from 'react-native';
+import { Pressable, StyleSheet, StatusBar, View} from 'react-native';
 
 import Text from '../Text';
 import { Link } from 'react-router-native';
@@ -14,9 +14,11 @@ const AppBarTab = ({text, onPressFunction, path}) => {
   return (
     // <View style={{marginLeft: 10}}>
     //   <Pressable styles={styles.container} onPress={onPressFunction}>
-    //     <Link to={path}>
-    //       <Text color="textSecondary">{text}</Text>
-    //     </Link>
+    //     {() => (
+    //       <Link to={path}>
+    //         <Text color="textSecondary">{text}</Text>
+    //       </Link>
+    //     )}
     //   </Pressable>
     // </View>
 
