ad6fa9dfc4aba21e938eee0c101e979d67bd8fee
rliu6915 <rliu6915@connect.hku.hk>
2024-01-07 19:32:25 +0800

    10.22/add component/Validate

--- src/components/SignUp/SignUpContainer.jsx
+++ src/components/SignUp/SignUpContainer.jsx
@@ -22,13 +22,18 @@ const SignUpContainer = ({ onSubmit }) => {
   const validationSchema = yup.object().shape({
     username: yup
       .string()
-      .required("Username is required"),
+      .required("Username is required")
+      .min(5, "Username must be at least 5 characters long")
+      .max(30, "Username must be at most 30 characters long"),
     password: yup
       .string()
-      .required("Password is required"),
+      .required("Password is required")
+      .min(5, "Username must be at least 5 characters long")
+      .max(50, "Username must be at most 50 characters long"),
     confirmation: yup
       .string()
-      .required("confirmation is required")
+      .required("Password confirmation is required")
+      .oneOf([yup.ref("password"), null], "Password confirmation matches the password")
   })
 
   return (
