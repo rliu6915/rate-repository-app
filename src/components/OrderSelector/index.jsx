b8006381bc23a4af00f0d1fc8dfd67215cd66aca
rliu6915 <rliu6915@connect.hku.hk>
2024-01-08 14:48:37 +0800

    10.23/implement orderSelector

--- src/components/OrderSelector/index.jsx
+++ src/components/OrderSelector/index.jsx
@@ -11,8 +11,19 @@ const styles = StyleSheet.create({
 
 const OrderSelector = () => {
   const [visible, setVisible] = useState(false)
+  const [order, setOrder] = useState('Latest repositories')
   const openMenu = () => setVisible(true)
   const closeMenu = () => setVisible(false)
+
+  const onPress = () => {
+    setOrder('Latest repositories')
+  }
+  const onPressOne = () => {
+    setOrder('Highest rated repositories')
+  }
+  const onPressTwo = () => {
+    setOrder('Lowest rated repositories')
+  }
   
   return (
     <PaperProvider>
@@ -20,12 +31,13 @@ const OrderSelector = () => {
         <Menu
           visible={visible}
           onDismiss={closeMenu}
-          anchor={<Button onPress={openMenu}>Show menu</Button>}
+          anchor={<Button onPress={openMenu} icon="camera">{order}</Button>}
         >
-          <Menu.Item onPress={() => {}} title="Item 1" />
-          <Menu.Item onPress={() => {}} title="Item 2" />
+          <Menu.Item title="Select an item..." disabled />
           <Divider />
-          <Menu.Item onPress={() => {}} title="Item 3" />
+          <Menu.Item onPress={onPress} title="Latest repositories" />
+          <Menu.Item onPress={onPressOne} title="Highest rated repositories" />
+          <Menu.Item onPress={onPressTwo} title="Lowest rated repositories" />
         </Menu>
       </View>
     </PaperProvider>
