bce8c46ca1796e4bd9e5c5741f274842cbc0a74d
rliu6915 <rliu6915@connect.hku.hk>
2024-01-09 09:47:47 +0800

    10.24/filter the repo list/query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -4,8 +4,8 @@ import { REPOSITORIES_DETAILS, REVIEW_DETAILS } from "./fragments"
 
 export const GET_REPOSITORIES = gql`
   ${REPOSITORIES_DETAILS}
-  query($orderDirection: OrderDirection, $orderBy: AllRepositoriesOrderBy)  {
-    repositories(orderDirection: $orderDirection, orderBy: $orderBy) {
+  query($orderDirection: OrderDirection, $orderBy: AllRepositoriesOrderBy, $searchKeyword: String)  {
+    repositories(orderDirection: $orderDirection, orderBy: $orderBy, searchKeyword: $searchKeyword) {
       edges {
         node {
           ...RepositoryDetails
