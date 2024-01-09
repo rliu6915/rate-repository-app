607d06833e1293c7775ea3ae9914d0af9ef47f96
rliu6915 <rliu6915@connect.hku.hk>
2024-01-09 11:54:44 +0800

    10.24/filter the repo list/implement search bar/

--- src/components/RepositoryList/RepositoryListContainer.jsx
+++ src/components/RepositoryList/RepositoryListContainer.jsx
@@ -10,6 +10,7 @@ import PressRepoItem from './PressRepoItem';
 import OrderSelector from "../OrderSelector"
 import SearchBar from "../SearchBar"
 // import RepositoryListHeader from "./RepositoryListHeader"
+import React from 'react';
 
 const styles = StyleSheet.create({
   separator: {
@@ -26,21 +27,57 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryListContainer = ({ repositories, order, setOrder, setOrderBy, setOrderDirection, setSearchQuery, searchQuery }) => {
-  // const { repositories } = useRepositories()
+// const RepositoryListContainer = ({ repositories, order, setOrder, setOrderBy, setOrderDirection, setSearchQuery, searchQuery }) => {
+//   // const { repositories } = useRepositories()
 
-  // Get the nodes from the edges array
-  const repositoryNodes = repositories
-  ? repositories.edges.map(edge => edge.node)
-  : []
+//   // Get the nodes from the edges array
+//   const repositoryNodes = repositories
+//   ? repositories.edges.map(edge => edge.node)
+//   : []
 
-  const listHeaderComponent = () => {
-    // return <RepositoryListHeader 
-    //   order={order}
-    //   setOrder={setOrder}
-    //   setOrderBy={setOrderBy}
-    //   setOrderDirection={setOrderDirection}
-    // />
+//   const listHeaderComponent = () => {
+//     // return <RepositoryListHeader 
+//     //   order={order}
+//     //   setOrder={setOrder}
+//     //   setOrderBy={setOrderBy}
+//     //   setOrderDirection={setOrderDirection}
+//     // />
+//     return (
+//       <View>
+//         <SearchBar
+//           searchQuery={searchQuery}
+//           setSearchQuery={setSearchQuery}
+//         />
+//         <OrderSelector 
+//           order={order} 
+//           setOrder={setOrder} 
+//           setOrderBy={setOrderBy} 
+//           setOrderDirection={setOrderDirection}
+//         />
+//       </View>
+//     )
+//   }
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList
+//         ListHeaderComponent={listHeaderComponent}
+//         ListHeaderComponentStyle={styles.listHeader}
+//         data={repositoryNodes}
+//         renderItem={({item}) => <PressRepoItem item={item} />}
+//         keyExtractor={item => item.id}
+//         ItemSeparatorComponent={ItemSeparator}
+//       />
+//     </SafeAreaView>
+//   )
+// }
+
+// export default RepositoryListContainer;
+
+class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    const { order, setOrder, setOrderBy, setOrderDirection, setSearchQuery, searchQuery } = this.props
+
     return (
       <View>
         <SearchBar
@@ -57,18 +94,25 @@ const RepositoryListContainer = ({ repositories, order, setOrder, setOrderBy, se
     )
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        ListHeaderComponent={listHeaderComponent}
-        ListHeaderComponentStyle={styles.listHeader}
-        data={repositoryNodes}
-        renderItem={({item}) => <PressRepoItem item={item} />}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={ItemSeparator}
-      />
-    </SafeAreaView>
-  )
+  render() {
+    const { repositories } = this.props
+    const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : []
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          ListHeaderComponent={this.renderHeader}
+          ListHeaderComponentStyle={styles.listHeader}
+          data={repositoryNodes}
+          renderItem={({item}) => <PressRepoItem item={item} />}
+          keyExtractor={item => item.id}
+          ItemSeparatorComponent={ItemSeparator}
+        />
+      </SafeAreaView>
+    )
+  }
 }
 
 export default RepositoryListContainer;
