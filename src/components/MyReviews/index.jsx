a7ffa0381b78dd433337263455a27173bf35b047
rliu6915 <rliu6915@connect.hku.hk>
2024-01-10 10:12:18 +0800

    10.25/component

--- src/components/MyReviews/index.jsx
+++ src/components/MyReviews/index.jsx
@@ -1,7 +1,47 @@
+import { useParams } from 'react-router-native'
+
+import { useQuery } from '@apollo/client'
+import { GET_REPOSITORY } from '../../graphql/queries'
+import { FlatList, Text, SafeAreaView, View, StyleSheet, StatusBar } from 'react-native'
+import ReviewItem from '../Repository/ReviewItem'
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+  container: {
+    flexDirection: "row",
+    marginTop: StatusBar.currentHeight || 0
+  }
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const MyReviews = () => {
+  // let { userId } = useParams()
+
+  // const { loading, error, data } = useQuery(GET_REPOSITORY, {
+  //   variables: { id: userId },
+  //   fetchPolicy: 'cache-and-network',
+  // })
+
+  // const repository = data ? data.repository : null
+  // if (loading) return <Text>Loading...</Text>
+  // if (error) return <Text>Error : {error}</Text>
+
+  // const reviewNodes = repository.reviews
+  // ? repository.reviews.edges.map(edge => edge.node)
+  // : []
+
   return (
-    null
+    <SafeAreaView >
+      <FlatList
+        data={reviewNodes}
+        renderItem={({item}) => <ReviewItem review={item} />}
+        keyExtractor={({ id }) => id}
+        ItemSeparatorComponent={ItemSeparator}
+      />
+    </SafeAreaView>
   )
 }
 
